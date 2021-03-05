579514f8c00620c27950449d0fe9f79bc4e7098e
lew <deathbykorea@gmail.com>
2021-03-05 23:25:14 +0000

    Added command line args, polished process starting
    Added support for command line args (see readme) 
    
    Dropped memtest support for now, can't seem to find a memtest utility I can call via the commandline that isn't paid 
    
    Program now waits for the test to exit before returning to the main menu, or closing if test is invoked through argument.

--- PCTesting/Program.cs
+++ PCTesting/Program.cs
@@ -3,15 +3,51 @@ using System.IO;
 using System.Threading;
 using System.Net;
 using System.Diagnostics;
+using CommandLine;
 namespace PCTesting
 {
+    public class Options
+    {
+        //Get commandline options, yay automation!
+        [Option('c',"cpu", Required = false, HelpText = "Start CPU test only.")]
+        public bool cpu { get; set;}
+        [Option('g', "gpu", Required = false, HelpText = "Start GPU test only.")]
+        public bool gpu { get; set; }
+        [Option('b', "both", Required = false, HelpText = "Start Combined test.")]
+        public bool both { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             //Set version and print copywrite
-            string version = "0.1.0";
+            string version = "0.2.0";
             Console.WriteLine("PC Testing Script v" + version + ". Made by Lew :) 2021");
+
+            //detect commandline args, shamelessly copy-pasted from the docs.
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.cpu)
+                    {
+                        Console.WriteLine("Starting CPU Only Test.");
+                        stressCPU(true);
+                    }
+                    else if (o.gpu)
+                    {
+                        Console.WriteLine("Starting GPU Only Test.");
+                        stressGPU(true);
+                    }
+                    else if (o.both)
+                    {
+                        Console.WriteLine("Starting Combined Test.");
+                        startBoth(true);
+                    }
+
+                });
+
+            //if no args detected, carry on and print menu :)
             Console.WriteLine("Please choose an option:");
             bool again = true;
             //Basic menu and option select
@@ -19,7 +55,6 @@ namespace PCTesting
             Console.WriteLine("1. CPU Stress Test");
             Console.WriteLine("2. GPU Stress Test");
             Console.WriteLine("3. Combined Stress Test");
-            Console.WriteLine("4. Memtest64 Memory Test");
             while (again)
             {
                 again = false;
@@ -27,18 +62,15 @@ namespace PCTesting
                 {
                 case "1":
                     Console.WriteLine("Starting CPU Stress Test");
-                        stressCPU();
+                        stressCPU(false);
                 break;
                 case "2":
                     Console.WriteLine("Starting Furmark GPU Test");
-                        stressGPU();
+                        stressGPU(false);
                 break;
                 case "3":
                     Console.WriteLine("Starting combined CPU/GPU Test");
-                        startBoth();
-                break;
-                case "4":
-                    Console.WriteLine("Starting Memtest64 Memory Test");
+                        startBoth(false);
                 break;
 
                 default:
@@ -48,7 +80,7 @@ namespace PCTesting
                  }
             }
         }
-        public static void stressCPU()
+        public static void stressCPU(bool arg)
         {
             //Check for cpu stress test, download if not, then run with -console option.
             Console.Clear();
@@ -62,13 +94,17 @@ namespace PCTesting
                 Console.WriteLine("File already exists, continuing!");
             }
             //Start CPU Stress test, pass args to PressureService to run in console mode. Press q to quit!
-            ProcessStartInfo startInfo = new ProcessStartInfo("cpustress.exe");
-            startInfo.WindowStyle = ProcessWindowStyle.Normal;
-            startInfo.Arguments = "-console";
-            Process.Start(startInfo);
+            Process process = new Process();
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            process.StartInfo.Arguments = "-console";
+            process.StartInfo.FileName = "cpustress.exe";
+            process.Start();
+            process.WaitForExit();
+            //If program invoked with cli arguments, exit after completition, otherwise, show menu.
+            if (arg) System.Environment.Exit(0);
         }
 
-        public static void stressGPU()
+        public static void stressGPU(bool arg)
         {
             //find if FurMark exists in either Program files or in testing tools directory
             string path = null;
@@ -77,15 +113,14 @@ namespace PCTesting
                 download("furmark.exe");
                 try
                 {
-                    //run installer, some of this is unneeded but idk what
+                    //run installer, some of this is unneeded but idk what ty stackoverflow
                     Console.WriteLine("Starting to install application");
-                    Process process = new Process();
-                    process.StartInfo.FileName = "furmark.exe";
-                    process.StartInfo.Arguments = string.Format(" /qb /i \"{0}\" ALLUSERS=1", "furmark.exe");
-                    process.StartInfo.Arguments = "/quiet";
-
-                    process.Start();
-                    process.WaitForExit();
+                    Process installer = new Process();
+                    installer.StartInfo.FileName = "furmark.exe";
+                    installer.StartInfo.Arguments = string.Format(" /qb /i \"{0}\" ALLUSERS=1", "furmark.exe");
+                    installer.StartInfo.Arguments = "/quiet";
+                    installer.Start();
+                    installer.WaitForExit();
                     Console.WriteLine("Application installed successfully!");
                 }
                 catch
@@ -106,28 +141,32 @@ namespace PCTesting
             }
             //start furmark with args
             Console.WriteLine("File already exists, continuing!");
+            Process process = new Process();
+
             ProcessStartInfo startInfo = new ProcessStartInfo(path);
-            startInfo.WindowStyle = ProcessWindowStyle.Normal;
-            startInfo.Arguments = "/nogui /width 1280 /height 720 /run_mode 2 /app_process_priority 50";
-            Process.Start(startInfo);
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            process.StartInfo.Arguments = "/nogui /width 1280 /height 720 /run_mode 2 /app_process_priority 50";
+            process.StartInfo.FileName = path;
+            process.Start();
 
             //set to high priority so it doesn't totally fall over when cpu bench is running
             Process[] processes = Process.GetProcessesByName("FurMark");
-            foreach (Process process in processes)
+            foreach (Process task in processes)
             {
-                process.PriorityClass = ProcessPriorityClass.High;
+                task.PriorityClass = ProcessPriorityClass.High;
             }
+            process.WaitForExit();
+            if (arg) System.Environment.Exit(0);
+
         }
 
-
-        
-
-        public static void startBoth()
+        public static void startBoth(bool arg)
         {
             //start both, does what it says on the tin
-            Thread CPU = new Thread(new ThreadStart(stressCPU));
-            Thread GPU = new Thread(new ThreadStart(stressGPU));
+            Thread CPU = new Thread(() => stressCPU(arg));
+            Thread GPU = new Thread(() => stressGPU(arg));
             GPU.Start();
+            // starting the cpu test too soon causes the gpu test to take way longer to load
             Thread.Sleep(5000);
             CPU.Start();
             GPU.Join();
@@ -136,8 +175,6 @@ namespace PCTesting
 
         public static void download(string name)
         {
-            // Kudzayi woz here
-
             //download file from my website. boo hoo hardcoded variables
             WebClient downloader = new WebClient();
             string uri = "https://d-bk.uk/files/", file;
